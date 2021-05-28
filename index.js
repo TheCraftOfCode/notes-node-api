d7d4d497db082e5d1c8ace7f0ce17a345c102d88
Manav Patnaik <manavpatnaik@hotmail.com>
2021-05-28 18:42:17 +0530

    Initialized Express app, configured dev env

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('Hello World')
+const express = require("express");
+const colors = require("colors");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+
+// Dev environment configuration
+morgan("tiny");
+dotenv.config();
+
+// Initialize app
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`.blue));
