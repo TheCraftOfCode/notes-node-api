76eb7f13649ecd277fdddf9de495d4a971b49af5
Manav Patnaik <manavpatnaik@hotmail.com>
2021-05-28 19:12:52 +0530

    Configured database connection

--- index.js
+++ index.js
@@ -2,13 +2,19 @@ const express = require("express");
 const colors = require("colors");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
+const connectDB = require("./config/db");
 
 // Dev environment configuration
 morgan("tiny");
 dotenv.config();
 
+// Connect to mongodb
+connectDB();
+
 // Initialize app
 const app = express();
+
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`.blue));
