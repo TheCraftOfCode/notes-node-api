3a54cc6d98e9f1688ec4fdd28f17ce23fcff5e8d
Manav Patnaik <manavpatnaik@hotmail.com>
2021-06-01 07:05:04 +0530

    Added timestamps on Note model

--- models/Note.js
+++ models/Note.js
@@ -1,21 +1,20 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-const NoteSchema = new mongoose.Schema({
+const NoteSchema = new mongoose.Schema(
+  {
     title: {
-        type: String,
-        required: [true, 'Note title is required'],
-        minlength: 6
+      type: String,
+      required: [true, 'Note title is required'],
+      minlength: 6,
     },
     content: {
-        type: String,
-        required: [true, 'Please provide note content']
+      type: String,
+      required: [true, 'Please provide note content'],
     },
-    createdDate: {
-        type: Date,
-        default: Date.now()
-    }
-});
+  },
+  { timestamps: true }
+);
 
-const Note = mongoose.model("Note", NoteSchema);
+const Note = mongoose.model('Note', NoteSchema);
 
 module.exports = Note;
