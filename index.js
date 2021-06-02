1144192d53c022a86d34c5eaee08bce76512b6dd
Manav Patnaik <manavpatnaik@hotmail.com>
2021-06-02 07:21:32 +0530

    Refactored main file

--- index.js
+++ index.js
@@ -4,9 +4,14 @@ const dotenv = require("dotenv");
 const morgan = require("morgan");
 const connectDB = require("./config/db");
 
+// Routes
+const notes = require("./routes/api/notes");
+
 // Dev environment configuration
-morgan("tiny");
 dotenv.config();
+if (process.env.NODE_ENV === "development") {
+  morgan("tiny");
+}
 
 // Connect to mongodb
 connectDB();
@@ -14,11 +19,15 @@ connectDB();
 // Initialize app
 const app = express();
 
+// Middleware
+app.use(express.json());
 
+// Routes Middleware
+app.use("/api/notes", notes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`.blue));
-app.get("/", function (req,res){
-	  res.send(`Hello, ${req.ip}!`)
-	console.log(`Request from ${req.ip}`)
-})
+app.get("/", function (req, res) {
+  res.send(`Hello, ${req.ip}!`);
+  console.log(`Request from ${req.ip}`);
+});
