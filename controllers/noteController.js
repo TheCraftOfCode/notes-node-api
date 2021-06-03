4ab5e188351bf7b8b12a7dda443ccc787cc4297d
Manav Patnaik <manavpatnaik@hotmail.com>
2021-06-03 14:36:46 +0530

    Backend complete

--- controllers/noteController.js
+++ controllers/noteController.js
@@ -1,12 +1,13 @@
 const Note = require("../models/Note");
 
 exports.getNotes = async (req, res) => {
-  const notes = await Note.find();
+  const notes = await Note.find({ user: req.user.id });
   res.status(200).json(notes);
 };
 
 exports.createNote = async (req, res) => {
   const note = new Note(req.body);
+  note.user = req.user.id;
   await note.save();
   res.status(201).json({ note });
 };
--- routes/notes.js
+++ routes/notes.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
 const { getNotes, createNote } = require("../controllers/noteController");
+const authenticateUser = require("../middleware/authenticateUser");
 
-router.get("/", getNotes);
-router.post("/", createNote);
+router.get("/", authenticateUser, getNotes);
+router.post("/", authenticateUser, createNote);
 
 module.exports = router;
