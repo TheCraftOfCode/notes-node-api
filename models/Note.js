3ad2e3c8185afda9a9f80af109806eac16b09a8a
Manav Patnaik <manavpatnaik@hotmail.com>
2021-06-04 16:46:07 +0530

    Added createdAt field to Note model

--- models/Note.js
+++ models/Note.js
@@ -1,24 +1,25 @@
 const mongoose = require("mongoose");
 
-const NoteSchema = new mongoose.Schema(
-  {
-    title: {
-      type: String,
-      required: [true, "Note title is required"],
-      minlength: 6,
-    },
-    content: {
-      type: String,
-      required: [true, "Please provide note content"],
-    },
-    user: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: [true, "Note creator must be present"],
-    },
+const NoteSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, "Note title is required"],
+    minlength: 6,
   },
-  { timestamps: true }
-);
+  content: {
+    type: String,
+    required: [true, "Please provide note content"],
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: [true, "Note creator must be present"],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
 
 const Note = mongoose.model("Note", NoteSchema);
 
